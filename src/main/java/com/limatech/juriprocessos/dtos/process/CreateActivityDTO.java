3ee48d429b93694bc1f18cf536ab8f5e1eb768bf
anti-duhring <mateus.limavn@gmail.com>
2023-04-16 21:01:11 -0300

    feat: Add activity service and controller

---
+++ src/main/java/com/limatech/juriprocessos/dtos/process/CreateActivityDTO.java
@@ -0,0 +1,62 @@
+package com.limatech.juriprocessos.dtos.process;
+
+import com.limatech.juriprocessos.models.process.Activity;
+import com.limatech.juriprocessos.models.process.Process;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class CreateActivityDTO {
+
+    private Process process;
+
+    private UUID processId;
+
+    private String name;
+
+    private String description;
+
+    private String type;
+
+    private LocalDateTime date;
+
+    public CreateActivityDTO(UUID processId, String name, String description, String type, LocalDateTime date) {
+        this.processId = processId;
+        this.name = name;
+        this.description = description;
+        this.type = type;
+        this.date = date;
+    }
+
+    public CreateActivityDTO() {
+
+    }
+
+    public Activity toEntity() {
+        return new Activity(process, name, description, type, date.toString());
+    }
+
+    public UUID getProcessId() {
+        return processId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setProcess(Process process) {
+        this.process = process;
+    }
+}
